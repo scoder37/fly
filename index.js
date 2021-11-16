2aac50ecf0125de1f34b88c31a12d191f0c3a6d4
Its Me Kayla <90820338+scoder37@users.noreply.github.com>
2021-11-16 14:41:04 +0700

    index

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+|=======================================|
+| NAME: NYOH API                        |
+| VERSION: 0.0.1                        |
+| CREATOR: AMIRUL DEV                   |
+| WEB: WWW.AMIRULDEV.NET                |
+| TEMPLATE BY: MBAH AGUS                |
+|=======================================|
+*/
+var express = require('express'),
+cors = require('cors'),
+secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./renz/lib/color.js')
+var mainrouter = require('./routes/main'),
+apirouter = require('./routes/web')
+
+var app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("./renz/views")) // temp
+
+app.use('/', mainrouter) // home
+app.use('/api', apirouter) // api
+
+app.listen(PORT, () => {
+console.log(color("Server running on port " + PORT,'green'))
+})
+
+module.exports = app
